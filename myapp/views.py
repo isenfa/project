830f3c4dc28776ba1c20df4d7c91554c960f8b30
thanadon <isenfa181249@gmail.com>
2026-07-06 10:23:13 +0700

    Add home page

---
+++ myapp/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, Vercel!")
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, Vercel!")
