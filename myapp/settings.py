d88ef4c9ef817b9e1eba3a85cce0bd312e9105dd
thanadon <isenfa181249@gmail.com>
2026-07-06 10:04:31 +0700

    Fix ALLOWED_HOSTS

--- myapp/settings.py
+++ myapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!f0x@wv!ddj29-2mhw4-!!icn5qc01u^77arzaq%*5_lebi_y7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
